ee4d150e8146ff05c03ddd924f5c914bc80dac53
RutbelCarlosTC <rttitoca@unsa.edu.pe>
2024-06-22 02:19:04 -0500

    acid java

---
+++ Database.java
@@ -0,0 +1,44 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @web https://www.jc-mouse.net/
+ * @author Mouse
+ */
+public class Database {
+    private final static String bd = "personas";
+    private final static String login = "root";
+    private final static String password = "1234";
+    private final static String url = "jdbc:mysql://localhost:3306/" + bd;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            Connection conn = DriverManager.getConnection(url, login, password);
+            if (conn != null) {
+                System.out.println("Conectado a la base de datos [" + bd + "]");
+            }
+            return conn;
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error al cargar el driver: " + e.getMessage());
+        } 
+        catch (SQLException e) {
+            System.out.println("Error al conectarse a la base de datos: " + e.getMessage());
+        } 
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = getConnection();
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+}
